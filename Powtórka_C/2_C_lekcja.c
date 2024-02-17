57d4c82ecce5856200e987be295d9f9ec4cc51de
Michal Banach <michal.banach@solarboat.agh.edu.pl>
2024-02-17 22:48:31 +0100

    ADD lesson 2_C

---
+++ Powtórka_C/2_C_lekcja.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int value;
+const int no =10 ;
+char a;
+
+int main()
+{
+    value=23;
+    a='L';
+
+
+    printf("to jest wynik zmiennerj value %d\n",value);
+    printf("to jest  to co chce wam powiedzieæ %c\n", a);
+     printf("to jest wynik zmiennerj value %d\n",no);
+
+      printf("to jest wynik zmiennerj value %d\n",no);
+
+    return 0;
+}
